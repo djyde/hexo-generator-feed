a70d8c984e478bc201ce73cd51abef787efa5de0
Randy <randypriv@gmail.com>
2016-01-05 14:33:26 +0800

    add itunes podcast feed support

--- index.js
+++ index.js
@@ -3,27 +3,34 @@ var pathFn = require('path');
 
 var config = hexo.config.feed = assign({
   type: 'atom',
-  limit: 20,
-  hub: '',
-  content: true
+  limit: 20
 }, hexo.config.feed);
 
 var type = config.type.toLowerCase();
 
 // Check feed type
-if (type !== 'atom' && type !== 'rss2') {
-  config.type = 'atom';
-} else {
-  config.type = type;
+switch(type){
+  case 'rss2':
+    config.type = 'rss2';
+    break;
+  case 'atom':
+    config.type = 'atom';
+    break;
+  case 'itunes':
+    config.type = 'itunes';
+    break;
+  default:
+    config.type = 'atom';
+    break;
 }
 
 // Set default feed path
-if (!config.path) {
+if (!config.path){
   config.path = config.type + '.xml';
 }
 
 // Add extension name if don't have
-if (!pathFn.extname(config.path)) {
+if (!pathFn.extname(config.path)){
   config.path += '.xml';
 }
 
